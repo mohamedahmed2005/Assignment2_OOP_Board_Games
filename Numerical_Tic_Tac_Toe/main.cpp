e670ca39b90e389669f593024e77a71a0992f3ff
mohamedahmed2005 <162487393+mohamedahmed2005@users.noreply.github.com>
2024-12-02 14:57:48 +0200

    Update main.cpp

--- Numerical_Tic_Tac_Toe/main.cpp
+++ Numerical_Tic_Tac_Toe/main.cpp
@@ -1,29 +1,178 @@
-#include "Numerical_Tic_Tac_Toe.h"
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#ifndef _BOARDGAME_CLASSES_H
+#define _BOARDGAME_CLASSES_H
 
+#include <string>
+#include <vector>
+#include<iomanip>
 using namespace std;
 
-int main() {
-    // Seed the random number generator for the random moves
-    srand(static_cast<unsigned int>(time(0)));
+template <typename T>
+class BoardGame_Classes {
+protected:
+    int rows, columns;
+    T** board;
+    int n_moves = 0;
 
-    // Create the board
-    Numerical_Tic_Tac_Toe<int> board;
+public:
 
-    // Create two players: Player 1 is a random player, Player 2 is a human player
-    Numerical_Tic_Tac_Toe_player<int> player1("Ahmed",0); // Odd player
-    Numerical_Tic_Tac_Toe_player<int> player2("Mohamed", 2); // Even player
 
-    // Create an array of players
-    Player<int>* players[2] = {&player1, &player2};
+    /// Return true if move is valid and put it on the board
+    /// within board boundaries in an empty cell
+    /// Return false otherwise
+    virtual bool update_board(int x, int y, T symbol) = 0;
 
-    // Create the game manager
-    Numerical_Game_manager<int> gameManager(&board, players);
+    /// Display the board and the pieces on it
+    virtual void display_board() = 0;
 
-    // Run the game
-    gameManager.run();
+    /// Returns true if there is any winner
+    virtual bool is_win() = 0;
 
-    return 0;
+    /// Return true if all moves are done and no winner
+    virtual bool is_draw() = 0;
+
+    /// Return true if the game is over
+    virtual bool game_is_over() = 0;
+};
+
+template <typename T>
+class Player {
+protected:
+    string name;
+    T symbol;
+    BoardGame_Classes<T>* boardPtr;  // Pointer to the board
+public:
+    /// Two constructors to initiate players
+    /// Give the player a symbol to use in playing
+    /// It can be X, O, or others
+    /// Optionally, you can give them a name
+    Player(string n, T symbol);
+    Player(T symbol); // For computer players
+
+    virtual void getmove(int& x, int& y) = 0;
+    T getsymbol();
+    string getname();
+    void setBoard(BoardGame_Classes<T>* b);
+};
+
+/// This class represents a random computer player
+/// that generates random positions x, y
+/// If invalid, the game manager asks to regenerate
+template <typename T>
+class RandomPlayer : public Player<T> {
+protected:
+    int dimension;
+public:
+    // Take a symbol and pass it to the parent
+    RandomPlayer(T symbol);
+    // Generate a random move
+    virtual void getmove(int& x, int& y) = 0;
+};
+
+template <typename T>
+class GameManager {
+private:
+    BoardGame_Classes<T>* boardPtr;
+    Player<T>* players[2];
+public:
+    GameManager(BoardGame_Classes<T>*, Player<T>* playerPtr[2]);
+
+    void run();
+};
+
+
+//--------------------------------------- IMPLEMENTATION
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+GameManager<T>::GameManager(BoardGame_Classes<T>* bPtr, Player<T>* playerPtr[2]) {
+    boardPtr = bPtr;
+    players[0] = playerPtr[0];
+    players[1] = playerPtr[1];
 }
+
+template <typename T>
+void GameManager<T>::run() {
+    int x, y;
+
+    boardPtr->display_board();
+
+    while (!boardPtr->game_is_over()) {
+        for (int i : {0, 1}) {
+            players[i]->getmove(x, y);
+            while (!boardPtr->update_board(x, y, players[i]->getsymbol())) {
+                players[i]->getmove(x, y);
+            }
+            boardPtr->display_board();
+            if (boardPtr->is_win()) {
+                cout << players[i]->getname() << " wins\n";
+                return;
+            }
+            if (boardPtr->is_draw()) {
+                cout << "Draw!\n";
+                return;
+            }
+        }
+    }
+}
+
+
+using namespace std;
+// Constructor for Player with a name and symbol
+template <typename T>
+Player<T>::Player(std::string n, T symbol) {
+    this->name = n;
+    this->symbol = symbol;
+}
+
+// Constructor for Player with only a symbol (e.g., for computer players)
+template <typename T>
+Player<T>::Player(T symbol) {
+    this->name = "Computer";
+    this->symbol = symbol;
+}
+
+// Constructor for RandomPlayer, passing the symbol to the parent Player class
+template <typename T>
+RandomPlayer<T>::RandomPlayer(T symbol) : Player<T>(symbol) {}
+
+// Getter for player's name
+template <typename T>
+std::string Player<T>::getname() {
+    return this->name;
+}
+
+// Getter for player's symbol
+template <typename T>
+T Player<T>::getsymbol() {
+    return this->symbol;
+}
+
+// Method to set the board pointer in the Player class
+template <typename T>
+void Player<T>::setBoard(BoardGame_Classes<T>* b) {
+    this->boardPtr = b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //_BOARDGAME_CLASSES_H
+
