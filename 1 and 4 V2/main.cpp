b66c470016c8801a699f0ca95f058e6f53a33e2f
Amr El-Dahshan <144555367+Amr-Khaled-Ahmed@users.noreply.github.com>
2024-12-04 21:10:19 +0200

    Add files via upload

---
+++ 1 and 4 V2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Pyramid.h"
+#include "BoardGame_Classes.h"
+#include "Word-Tic-tac-toe.h"
+using namespace std;
+
+int main() {
+    PyramidGame();
+//    Word_Tic_tac_toe();
+    return 0;
+}
+
+
