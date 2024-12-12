302ab2c914336154c9b27ae8df67e9e71373e6c4
Amr El-Dahshan <144555367+Amr-Khaled-Ahmed@users.noreply.github.com>
2024-12-12 18:58:27 +0200

    Update pyramid.h

--- 1 and 4 games/pyramid.h
+++ 1 and 4 games/pyramid.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include "BoardGame_Classes.h"
+#include <cctype>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,7 +18,6 @@ class PyramidBoard : public Board<T> {
 private:
 
     bool is_within_bounds(int x, int y);  // Check if a position is within bounds
-    bool check_line(int x, int y, T symbol);  // Check line for win condition
 
 public:
     PyramidBoard();  // Constructor
@@ -62,46 +63,34 @@ bool PyramidBoard<T>::is_within_bounds(int x, int y) {
     return x >= 0 && x < this->rows && y >= 0 && y < 2 * x + 1;
 }
 
-template <typename T>
-bool PyramidBoard<T>::check_line(int x, int y, T symbol) {
-    // Check horizontal line
-    if (y - 2 >= 0 && this->board[x][y - 2] == symbol && this->board[x][y - 1] == symbol && this->board[x][y] == symbol) {
-        return true;
-    }
-
-    // Check diagonal lines (both directions)
-    if (x + 2 < this->rows && y - 2 >= 0 &&
-        this->board[x + 1][y - 1] == symbol && this->board[x + 2][y - 2] == symbol) {
-        return true;
-    }
-
-    if (x + 2 < this->rows && y + 2 < 2 * (x + 2) + 1 &&
-        this->board[x + 1][y + 1] == symbol && this->board[x + 2][y + 2] == symbol) {
-        return true;
-    }
-
-    if (x + 2 < this->rows && y - 2 >= 0 &&
-        this->board[x + 1][y + 2] == symbol && this->board[x + 2][y + 3] == symbol) {
-        return true;
-    }
-    return false;
-}
 
 template <typename T>
 bool PyramidBoard<T>::is_win() {
-    for (int i = 0; i < this->rows; ++i) {
-        // Loop through each element in the current row
-        for (int j = 0; j < 2 * i + 1; ++j) {
-            // Only check if the symbol is not '-'
-            if (this->board[i][j] != '-') {
-                // Check for a winning line starting from (i, j)
-                if (check_line(i, j, this->board[i][j])) {
-                    return true;  // A win is detected
-                }
-            }
-        }
+    // horizontal
+    if ((this->board[0][0] == this->board[1][1] && this->board[1][1] == this->board[2][2] && this->board[0][0] != '-')){
+        return true;
     }
-    return false;  // No win detected
+    //vertical cases
+    // second row
+    if ((this->board[1][0] == this->board[1][1] && this->board[1][1] == this->board[1][2] && this->board[1][0] != '-')){
+        return true;
+    }
+    // third row
+    if ((this->board[2][0] == this->board[2][1] && this->board[2][1] == this->board[2][2] && this->board[2][0] != '-')){
+        return true;
+    }if ((this->board[2][1] == this->board[2][2] && this->board[2][2] == this->board[2][3] && this->board[2][3] != '-')){
+        return true;
+    }if ((this->board[2][2] == this->board[2][3] && this->board[2][3] == this->board[2][4] && this->board[2][4] != '-')){
+        return true;
+    }
+
+    // Check diagonals
+    if ((this->board[0][0] == this->board[1][0] && this->board[1][0] == this->board[2][0] && this->board[2][0] != '-') ||
+        (this->board[0][0] == this->board[1][2] && this->board[1][2] == this->board[2][4] && this->board[2][4] != '-')) {
+        return true;
+    }
+
+    return false;
 }
 
 
@@ -110,7 +99,7 @@ bool PyramidBoard<T>::is_win() {
 template <typename T>
 bool PyramidBoard<T>::update_board(int x, int y, T symbol) {
     if (!is_within_bounds(x, y) || this->board[x][y] != '-') {
-        cout << "Invalid move. Try again.\n";
+//        cout << "Invalid move. Try again.\n";
         return false;
     }
     this->board[x][y] = symbol;
@@ -157,6 +146,8 @@ bool PyramidBoard<T>::game_is_over() {
 
 template <typename T>
 class PyramidPlayer : public Player<T>{
+private:
+    T** boardPTR2D;
 public:
     PyramidPlayer(string n, T symbol) ;
     void getmove(int& x, int& y) override;
@@ -171,11 +162,33 @@ PyramidPlayer<T>::PyramidPlayer(string n, T symbol) : Player<T>(n, symbol) {}
 
 template <typename T>
 void PyramidPlayer<T>::getmove(int& x, int& y) {
-    // Implementation for getting the move (can be extended)
-    cout << "Enter the row\n";
-    cin >> x;
-    cout << "Enter the column\n";
-    cin >> y;
+    string strX, strY;
+    cout << this->name << " turn " << endl;
+    while (true) {
+        cout << "Enter the row: ";
+        cin >> strX;
+
+        // Check if the input is numeric
+        if (!strX.empty() && all_of(strX.begin(), strX.end(), ::isdigit)) {
+            x = stoi(strX); // Convert string to integer
+            break;
+        } else {
+            cout << "Invalid input. Please enter a valid numeric row.\n";
+        }
+    }
+
+    while (true) {
+        cout << "Enter the column: ";
+        cin >> strY;
+
+        // Check if the input is numeric
+        if (!strY.empty() && all_of(strY.begin(), strY.end(), ::isdigit)) {
+            y = stoi(strY); // Convert string to integer
+            break;
+        } else {
+            cout << "Invalid input. Please enter a valid numeric column.\n";
+        }
+    }
 }
 
 
@@ -184,97 +197,113 @@ void PyramidPlayer<T>::getmove(int& x, int& y) {
 template <typename T>
 class PyramidRandomPlayer : public RandomPlayer<T> {
 public:
-    PyramidRandomPlayer(T symbol);
+    PyramidRandomPlayer(string n,T symbol);
 
     // Generates a random move for the pyramid board.
     void getmove(int& x, int& y);
 };
 
 template <typename T>
-PyramidRandomPlayer<T>::PyramidRandomPlayer(T symbol) : RandomPlayer<T>(symbol) {
+PyramidRandomPlayer<T>::PyramidRandomPlayer(string n, T symbol) : RandomPlayer<T>(symbol) {
     this->dimension = 3;
-    this->name = "Mr. Habida";
+    this->name = n;
     srand(static_cast<unsigned int>(time(0)));
 }
 
 template <typename T>
 void PyramidRandomPlayer<T>::getmove(int& x, int& y) {
-    x = rand() % 2;
-    y = rand() % 4;
+    cout << this->name << " turn " << endl;
+    x = rand() % 3;
+    y = rand() % 5;
+
 }
 
 
-/// ---------------------------------------------------------------- AI_Min_Max Pyramid player
-
-
-
-
-
-
-
-
-
-
-
 /// ---------------------------------------------------------------- Implementation Function
-void PyramidGame(){
+void PyramidGame() {
     string choice;
     Player<char>* players[2];
     Board<char>* pyramid_board = new PyramidBoard<char>();
     string player1Name, player2Name;
-    char s1,s2;
+    char s1, s2;
+
     cout << "Welcome to my Pyramid X-O Game.\n";
 
-    cout << "Enter Player 1 name: ";
-    cin >> player1Name;
+    // Set up Player 1
     do {
-        cout << "Please choose your symbol\n" << "1. to select 'X'\n" << "2. to select 'O'\n";
+        cout << "Choose Player 1 type:\n";
+        cout << "1. Human\n";
+        cout << "2. Random Computer\n";
+        cout << "Enter your choice: ";
         cin >> choice;
-        if(choice == "1"){
-            s1 = 'X';
-            s2 = 'O';
+        do {
+            cout << "Choose your symbol:\n";
+            cout << "1. 'X'\n";
+            cout << "2. 'O'\n";
+            cin >> choice;
+
+            if (choice == "1") {
+                s1 = 'X';
+                s2 = 'O';
+                break;
+            } else if (choice == "2") {
+                s1 = 'O';
+                s2 = 'X';
+                break;
+            } else {
+                cout << "Invalid choice. Please choose 1 or 2.\n";
+            }
+        } while (true);
+        if (choice == "1" || choice == "2" || choice == "3") {
+            cout << "Enter Player 1 name: ";
+            cin >> player1Name;
+            if (choice == "1") {
+                players[0] = new PyramidPlayer<char>(player1Name, s1);
+            } else if (choice == "2") {
+                players[0] = new PyramidRandomPlayer<char>(player1Name, s1);
             break;
-        }else if(choice == "2"){
-            s1 = 'O';
-            s2 = 'X';
-            break;
-        }else{
+        } else {
             cout << "Invalid choice. Please try again.\n";
         }
-    }while (true);
-    players[0] = new PyramidPlayer<char>(player1Name, s1);
+    } while (true);
 
     // Ensure Player 2 has a unique name
-    while (player2Name == player1Name) {
-        cout << "Player 2 name cannot be the same as Player 1. Enter a different name: ";
+    do {
+        cout << "Enter Player 2 name (different from Player 1): ";
         cin >> player2Name;
-    }
+        if (player2Name != player1Name) {
+            break;
+        } else {
+            cout << "Player 2 name cannot be the same as Player 1. Try again.\n";
+        }
+    } while (true);
 
+    // Set up Player 2
     do {
         cout << "Choose Player 2 type:\n";
         cout << "1. Human\n";
         cout << "2. Random Computer\n";
+        cout << "Enter your choice: ";
         cin >> choice;
+
         if (choice == "1") {
-            // Set up Player 2
-            cout << "Enter Player 2 name: ";
-            cin >> player2Name;
             players[1] = new PyramidPlayer<char>(player2Name, s2);
             break;
-        }else if(choice == "2") {
-            players[1] = new PyramidRandomPlayer<char>(s2);
+        } else if (choice == "2") {
+            players[1] = new PyramidRandomPlayer<char>(player2Name, s2);
             break;
-        }else {
-            cout << "Invalid choice for Player 2. Please try again.\n";
+        } else {
+            cout << "Invalid choice. Please try again.\n";
         }
-    }while (true);
-
+    } while (true);
 
     // Assign board to players
     players[0]->setBoard(pyramid_board);
     players[1]->setBoard(pyramid_board);
-    cout << "(" << players[0]->getname() << "( with symbol " << players[0]->getsymbol() << " _vs_ ("
-         << players[1]->getname() << ") with symbol " << players[1]->getsymbol() << endl;
+
+    // Display game setup
+    cout << "(" << players[0]->getname() << ") with symbol (" << players[0]->getsymbol() << ") vs ("
+         << players[1]->getname() << ") with symbol (" << players[1]->getsymbol() << ")\n";
 
     // Run the game
     GameManager<char> pyramidGame(pyramid_board, players);
@@ -284,7 +313,6 @@ void PyramidGame(){
     delete players[0];
     delete players[1];
     delete pyramid_board;
-
-    return;
 }
+
 #endif
